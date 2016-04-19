0db85bc4d49677cd658bbdaebff18a6dd950619d
Rob Dimsdale <rdimsdale@pivotal.io>
2016-04-19 11:02:29 -0700

    Add script to regenerate checkman.
    - also run the script to pick up latest changes.
    
    [#117745915]

---
+++ update.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eux
+
+function add_canibump() {
+  cat <<EOF > canibump
+$(cat canibump)
+canibump: curl -f -s https://canibump.cfapps.io | grep -q YES <(grep -A1 'id="can_i_bump"') && echo '{"result":true,"changing":false,"info":[],"url":"https://canibump.cfapps.io"}' || echo '{"result":false,"changing":false,"info":[],"url":"https://canibump.cfapps.io"}'
+EOF
+}
+
+function remove_rubbish_bin() {
+  sed -i '' '/rubbish-bin/d' cf-release
+  sed -i '' '/deploy-and-test-a1/d' cf-release
+  sed -i '' '/a1-diego-deploy-and-test/d' cf-release
+  sed -i '' '/deploy-and-test-vsphere/d' cf-release
+}
+
+function main() {
+  local pipelines
+  pipelines=("alfredo" "alfredo-canaries" "canibump" "cf-release" "cf-release-final" "mkman" "multierror" "runtime-ci" "runtime-dev-envs")
+
+  for pipeline in ${pipelines[@]}; do
+    fly -t runtime checklist -p "${pipeline}" > "${pipeline}"
+  done
+
+  add_canibump
+
+  remove_rubbish_bin
+}
+
+main
