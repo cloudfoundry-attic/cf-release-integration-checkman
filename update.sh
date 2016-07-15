5fe6b7ce5433abef15c3763bb4289dc913fcd1aa
Amit Kumar Gupta <agupta@pivotal.io>
2016-07-15 12:51:22 -0700

    update update.sh, add datadog Checkfile

--- update.sh
+++ update.sh
@@ -11,7 +11,7 @@ EOF
 
 function main() {
   local pipelines
-  pipelines=("canibump" "cf-release" "cf-release-final" "runtime-ci" "runtime-dev-envs" "build-docker-images" "nats-release" "cf-bosh-2-0")
+  pipelines=("canibump" "cf-release" "cf-release-final" "runtime-ci" "runtime-dev-envs" "build-docker-images" "nats-release" "cf-bosh-2-0" "datadog")
 
   for pipeline in ${pipelines[@]}; do
     fly -t runtime-ci checklist -p "${pipeline}" > "${pipeline}"
